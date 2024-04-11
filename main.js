ac47c3443e8b935cfc1cf1d3010680f1a069cffc
Ewert Daniel (CR/APT5) <daniel.ewert@bosch.com>
2024-04-11 14:17:42 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css'
+
+// read url params
+const urlParams = new URLSearchParams(window.location.search)
+const number = urlParams.get('nummer')
+
+// fetch data file
+fetch(`/data/${number}.json`)
+  .then(response => response.json())
+  .then(data => {
+    // set hinweisnr
+    document.getElementById('hinweisnr').innerText = number
+    // set zauberzahl
+    document.getElementById('zauberzahl').innerText = data.zauberzahl
+    // set hinweis
+    document.getElementById('hinweis').innerText = data.hinweis
+  })
+
+
+
