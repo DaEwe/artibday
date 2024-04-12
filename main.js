01bbb55f2c7ef0ee921b3890d08515d535815609
Ewert Daniel (CR/APT5) <daniel.ewert@bosch.com>
2024-04-13 00:21:48 +0200

    Fix import statement formatting in main.js

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-import './style.css'
+import "./style.css";
 
 // read url params
-const urlParams = new URLSearchParams(window.location.search)
-const number = urlParams.get('nummer')
+const urlParams = new URLSearchParams(window.location.search);
+const number = urlParams.get("nummer") | 1;
 
 // fetch data file
 fetch(`/data/${number}.json`)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     // set hinweisnr
-    document.getElementById('hinweisnr').innerText = number
+    document.getElementById("hinweisnr").innerText = number;
     // set zauberzahl
-    document.getElementById('zauberzahl').innerText = data.zauberzahl
+    document.getElementById("zauberzahl").innerText = data.zauberzahl;
     // set hinweis
-    document.getElementById('hinweis').innerText = data.hinweis
-  })
-
-
-
+    document.getElementById("hinweis").innerText = data.hinweis;
+  });
